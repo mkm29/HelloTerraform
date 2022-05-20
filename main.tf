0322009f3b87ab2dd92e49e83762fc169d7bec6e
Mitchell Murphy <mitchellmurphy@macbook-pro.local>
2022-05-20 14:45:35 -0400

    Added IGW to the VPC, a route table and associated the RT with the subnet

--- main.tf
+++ main.tf
@@ -21,17 +21,40 @@ resource "local_sensitive_file" "private_key" {
   filename = "./id_rsa"
 }
 
-resource "local_sensitive_file" "public_key" {
-  # write contents of privarte key to file
-  content  = tls_private_key.ec2_key.public_key_openssh
-  filename = "./id_rsa.pub"
-}
+# resource "local_sensitive_file" "public_key" {
+#   # write contents of privarte key to file
+#   content  = tls_private_key.ec2_key.public_key_openssh
+#   filename = "./id_rsa.pem"
+# }
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.30.0.0/16"
   tags = {
     Name = "terraform-vpc"
   }
+  # enable dns hostnames and dns support
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+# create an internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# create route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rt_associate_subnet" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_security_group" "ec2_sg" {
